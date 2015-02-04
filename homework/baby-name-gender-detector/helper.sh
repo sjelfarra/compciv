0dca8d1ddba0d27e9609bd412f7e02ed8d8abd0e
Sabrina Jane Elfarra <sabrinae@corn22.stanford.edu>
2015-02-04 14:31:07 -0800

    Assignment is done.

---
+++ homework/baby-name-gender-detector/helper.sh
@@ -0,0 +1,6 @@
+cd baby-name-gender-detector 
+mkdir -p ./data-hold
+cd data-hold
+curl -o names.zip http://stash.compciv.org/ssa_baby_names/names.zip
+unzip -p names.zip yob1973.txt yob2013.txt | dos2unix > namesample.txt
+cd ..
